45a5c8c820e5ba6cafde3e73ca723aeaf85994a4
Junzhezhang1031 <zjzlalala123@163.com>
2025-04-27 15:12:59 +0930

    change variables

--- sr.c
+++ sr.c
@@ -23,12 +23,11 @@
 **********************************************************************/
 
 #define RTT  16.0       /* round trip time.  MUST BE SET TO 16.0 when submitting assignment */
-#define WINDOWSIZE 6    /* the maximum number of buffered unacked packet */
+#define WINDOWSIZE 5    /* the maximum number of buffered unacked packet */
 #define SEQSPACE 7      /* the min sequence space for GBN must be at least windowsize + 1 */
 #define NOTINUSE (-1)   /* used to fill header fields that are not being used */
 
 
-#define WINDOW_SIZE 5
 #define BUFFER_SIZE 50  
 
 struct pkt send_buffer[BUFFER_SIZE];  
