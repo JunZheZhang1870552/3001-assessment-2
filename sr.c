35c76e29e4972f962b6c5cfbfd5f1cb0e48eb5ae
Junzhezhang1031 <zjzlalala123@163.com>
2025-05-04 22:32:57 +0930

    debug38

--- sr.c
+++ sr.c
@@ -256,7 +256,7 @@ void B_input(struct pkt packet)
   if (IsCorrupted(packet)) {
     /* packet is corrupted or out of order resend last ACK */
     if (TRACE > 0)
-      printf("----B: packet corrupted or not expected sequence number, resend ACK!\n")
+      printf("----B: packet corrupted or not expected sequence number, resend ACK!\n");
     ack.seqnum = 0;
     ack.acknum = (expectedseqnum + SEQSPACE - 1) % SEQSPACE;
     for (i = 0; i < 20; i++) ack.payload[i] = 0;
